6593b28d83a929bb34dab7b5e25d3da93fae0a35
shuvoamin@yahoo.com <shuvoamin@yahoo.com>
2015-11-30 17:00:52 +0000

    initial commit/push

---
+++ LinqSamples/SimpleLambdas/Program.cs
@@ -0,0 +1,15 @@
+﻿//Copyright (C) Microsoft Corporation.  All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+// See the ReadMe.html for additional information
+class Program
+{
+    static void Main()
+    {
+        Samples.Sample1();        
+        Console.ReadLine();
+    }
+}
